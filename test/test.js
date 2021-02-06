9bbc617e3498d61508787bc0ceda2a984621285c
Rajas1312 <you@example.com>
2021-02-06 18:31:35 +0530

    Refactored: completed all the test cases for length

--- test/test.js
+++ test/test.js
@@ -32,11 +32,21 @@ describe('positive quantity analysis', () => {
 
     })
 
-    it('given lengths when equal should return true', () => {
+    it('given addition lengths when equal should pass the test', () => {
         assert.equal(quantityTesting.addLengths(unit.getInch1() * 2, unit.getInch1() * 2), unit.getInch1() * 4);
-
     })
 
+    it('given addition lengths when equal should pass the test', () => {
+        assert.equal(quantityTesting.addLengths(unit.getFoot() * 1, unit.getInch() * 2), unit.getInch() * 14);
+    })
+
+    it('given addition lengths when equal should pass the test', () => {
+        assert.equal(quantityTesting.addLengths(unit.getFoot() * 1, unit.getFoot() * 1), unit.getInch() * 24);
+    })
+
+    it('given addition lengths when equal should pass the test', () => {
+        assert.equal(quantityTesting.addLengths(unit.getInch1() * 2, unit.getCentimeter() * 2.5), unit.getInch1() * 3);
+    })
 
 
 
